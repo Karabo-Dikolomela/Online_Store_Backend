121814d8ff69d35f991e94d06476e612426256a1
Karabo-Dikolomela <108591320+Karabo-Dikolomela@users.noreply.github.com>
2022-12-12 15:06:17 +0200

    created data page and added styling to buttons

---
+++ inc/data.inc.php
@@ -0,0 +1,11 @@
+<?php
+
+$bookData = [
+
+    new Book("Game of Thrones", "Fantasy and Fiction", 170, "https://images.unsplash.com/photo-1515255384510-23e8b6a6ca3c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1032&q=80", 1),
+    new Book("MockingJay", "Science Fiction", 150, "", 1),
+    new Book("Catching fire", "Science Fiction", 227, "", 1),
+    new Book("Twilight", "Fantasy and Fiction", 175, "", 1),
+    new Book("The Ballad of Songbirds and Snakes", "Science Fiction", 244, "", 1)
+
+];
