b2d20558ad8bd48fec7e93c5d7deb52c272607bb
Karabo-Dikolomela <108591320+Karabo-Dikolomela@users.noreply.github.com>
2022-12-12 06:02:23 +0200

    Create footer page
    Includes more links

---
+++ inc/footer.php
@@ -0,0 +1,41 @@
+<div class="container-fluid bg-white mt-5">
+
+<div class="row">
+    <div class="col-lg-4">
+
+
+        <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+            Quo illo dolores quisquam rerum numquam suscipit accusamus vitae eveniet explicabo,
+            ad enim eligendi tempora unde aspernatur ea similique omnis dicta a?
+        </p>
+
+        <img class="navbar-brand w-0" src="images/logo/logo.jpg" width="150" height="104" alt="logo">
+    </div>
+    <div class="col-lg-4 p-4">
+        <h5 class="mb-3">Links</h5>
+        <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Home</a><br>
+        <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">New</a><br>
+        <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Books</a><br>
+        <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Shop</a><br>
+        <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Contact us</a><br>
+        <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">About</a><br>
+    </div>
+    <div class="col-lg-4 p-4">
+        <h5 class="mb-3">Follow us</h5>
+        <a href="" class="d-inline-block text-dark text-decoration-none mb-2">
+            <i class="bi bi-twitter me-1"> Twitter </i>
+        </a><br>
+        <a href="" class="d-inline-block text-dark text-decoration-none mb-2">
+            <i class="bi bi-facebook me1"> Facebook </i>
+        </a><br>
+        <a href="" class="d-inline-block text-dark text-decoration-none">
+            <i class="bi bi-instagram me-1"> Instagram </i>
+        </a><br>
+    </div>
+</div>
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+<!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
