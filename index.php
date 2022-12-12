34d0b098a6f0545e1bb280741f83cd325110c14c
Karabo-Dikolomela <108591320+Karabo-Dikolomela@users.noreply.github.com>
2022-12-12 06:59:42 +0200

    added feature section on landing page

--- index.php
+++ index.php
@@ -70,6 +70,127 @@
         </div>
     </div>
 
+     <!-- Featured Books -->
+     <h2 class="mt-5 pt-4 mb-4 text-center fw-bold">FEATURED</h2>
+
+     <div class="container">
+        <div class="row">
+            <div class="col-lg-4 col-md-6 my-3">
+                <div class="card border-0 shadow" style="max-width: 350px; margin: auto;">
+                    <img src="./images/fiction/2.jpg" class="">
+                    <div class="card-body">
+                        <h5>YOUR SOUL IS A RIVER</h5>
+                        <h6 class="mb-4">R200</h6>
+                        <div class="features mb-4">
+                            <h6 class="mb-1">Details</h6>
+                            <span class="badge rounded-pill bg-light text-dark text-wrap">
+                                <strong>Author: </strong>  Nikita Gil
+                            </span>
+                            <span class="badge rounded-pill bg-light text-dark text-wrap">
+                                <strong>Genre:</strong>Self-Help
+                            </span>
+                            <span class="badge rounded-pill bg-light text-dark text-wrap">
+                                <strong>pages:</strong> 602
+                            </span>
+                        </div>
+
+                        <div class="rating mb-4">
+                            <h6 class="mb-1">Rating</h6>
+                            <span class="badge round-pill bg-light">
+                                <i class="bi bi-star-fill text-warning"></i>
+                                <i class="bi bi-star-fill text-warning"></i>
+                                <i class="bi bi-star-fill text-warning"></i>
+                                <i class="bi bi-star-fill text-warning"></i>
+                                <i class="bi bi-star-fill text-warning"></i>
+                            </span>
+                        </div>
+                        <div class="d-flex justify-content-evenly mb-2">
+                            <a href="#" class="btn btn-sn text-white custom-bg shadow-none">Add to Cart</a>
+                            <a href="#" class="btn btn-sn btn-outline-dark shadow-none">More details</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 my-3">
+                <div class="card border-0 shadow" style="max-width: 350px; margin: auto;">
+                    <img src="./images/fiction/1.jpg" class="card-img-top">
+                    <div class="card-body">
+                        <h5>CEREAL</h5>
+                        <h6 class="mb-4">R200</h6>
+                        <div class="features mb-4">
+                            <h6 class="mb-1">Details</h6>
+                            <span class="badge rounded-pill bg-light text-dark text-wrap">
+                                <strong>Author:</strong> Vincent Van Duysen
+                            </span>
+                            <span class="badge rounded-pill bg-light text-dark text-wrap">
+                                <strong>Genre:</strong> Self-Help
+                            </span>
+                            <span class="badge rounded-pill bg-light text-dark text-wrap">
+                                <strong>pages:</strong> 900
+                            </span>
+
+                        </div>
+
+                        <div class="rating mb-4">
+                            <h6 class="mb-1">Rating</h6>
+                            <span class="badge round-pill bg-light">
+                                <i class="bi bi-star-fill text-warning"></i>
+                                <i class="bi bi-star-fill text-warning"></i>
+                                <i class="bi bi-star-fill text-warning"></i>
+                                <i class="bi bi-star-fill text-warning"></i>
+                                <i class="bi bi-star-fill text-warning"></i>
+                            </span>
+                        </div>
+                        <div class="d-flex justify-content-evenly mb-2">
+                            <a href="#" class="btn btn-sn text-white custom-bg shadow-none">Add to Cart</a>
+                            <a href="#" class="btn btn-sn btn-outline-dark shadow-none">More details</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 my-3">
+                <div class="card border-0 shadow" style="max-width: 350px; margin: auto;">
+                    <img src="images/Self_Help_Books/i-self-help.jpg" class="card-img-top">
+                    <div class="card-body">
+                        <h5>FALLING TOWARD THE MOON</h5>
+                        <h6 class="mb-4">R200</h6>
+                        <div class="features mb-4">
+                            <h6 class="mb-1">Details</h6>
+                            <span class="badge rounded-pill bg-light text-dark text-wrap">
+                                <strong>Author:</strong> Robert M.Drake
+                            </span>
+                            <span class="badge rounded-pill bg-light text-dark text-wrap">
+                                <strong>Genre:</strong> Self Help
+                            </span>
+                            <span class="badge rounded-pill bg-light text-dark text-wrap">
+                                <strong>pages:</strong> 850
+                            </span>
+                        </div>
+
+                        <div class="rating mb-4">
+                            <h6 class="mb-1">Rating</h6>
+                            <span class="badge round-pill bg-light">
+                                <i class="bi bi-star-fill text-warning"></i>
+                                <i class="bi bi-star-fill text-warning"></i>
+                                <i class="bi bi-star-fill text-warning"></i>
+                                <i class="bi bi-star-fill text-warning"></i>
+                                <i class="bi bi-star-fill text-warning"></i>
+                            </span>
+                        </div>
+                        <div class="d-flex justify-content-evenly mb-2">
+                            <a href="#" class="btn btn-sn text-white custom-bg shadow-none">Add to Cart</a>
+                            <a href="#" class="btn btn-sn btn-outline-dark shadow-none">More details</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-12 text-center mt-5">
+                <a href="#" class="btn btn-sm btn-outline-dark rounded-0 fw-bold shadow-none">More Books >>></a>
+            </div>
+        </div>
+    </div>
+
 <?php require('inc/footer.php') ?>
 
 <script src="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js"></script>
