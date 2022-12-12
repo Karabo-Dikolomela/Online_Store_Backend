fece831efa4467b339f57ac07d3575859b973d17
Karabo-Dikolomela <108591320+Karabo-Dikolomela@users.noreply.github.com>
2022-12-12 15:18:38 +0200

    Created Look book page

---
+++ books.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> - BOOKS</title> 
+    <?php require_once('inc/links.php') ?>
+    <style>
+        .pop:hover{
+            border-top-color: var(--teal) !important;
+            transform: scale(1.03);
+            transition: all 0.4s;
+        }
+    </style>
+</head>
+
+<body class="bg-light">
+
+    <?php require_once('inc/header.php') ?>
+
+    <div class="my-5 px-4">
+        <h2 class="fw-bold text-center">BOOKS</h2>
+        <div class="h-line bg-dark"></div>
+        <p class="text-center mt-3">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Nostrum provident rem id quasi quis est nisi quidem totam
+            saepe magni culpa tenetur tempora mzodi quos nobis distinctio libero, quam facere.
+        </p>
+    </div>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4 col-md-6 mb-5 px-5">
+                <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+                    <div class="d-flex-row align-items-center mb-2">
+                        <img src="images/Book/a.jpg " width="200px">
+                        <h6 class="mb-4">R200</h6>
+                            <a href="#" class="btn btn-sn w-100 text-white custom-bg shadow-none mb-2">Add to Cart</a>
+                            <a href="#" class="btn btn-sn w-100 btn-outline-dark shadow-none">More details</a>
+                        <h5 class="mb-0 ms-0"></h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 mb-5 px-5">
+                <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+                    <div class="d-flex-row align-items-center mb-2">
+                        <img src="images/Book/b.jpg" width="200px">
+                        <h6 class="mb-4">R200</h6>
+                            <a href="#" class="btn btn-sn w-100 text-white custom-bg shadow-none mb-2">Add to Cart</a>
+                            <a href="#" class="btn btn-sn w-100 btn-outline-dark shadow-none">More details</a>
+                        <h5 class="mb-0 ms-0"></h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 mb-5 px-5">
+                <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+                    <div class="d-flex-row align-items-center mb-2">
+                        <img src="images/Book/c.jpg " width="200px">
+                        <h6 class="mb-4">R200</h6>
+                            <a href="#" class="btn btn-sn w-100 text-white custom-bg shadow-none mb-2">Add to Cart</a>
+                            <a href="#" class="btn btn-sn w-100 btn-outline-dark shadow-none">More details</a>
+                        <h5 class="mb-0 ms-0"></h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 mb-5 px-5">
+                <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+                    <div class="d-flex-row align-items-center mb-2">
+                        <img src="images/Book/d.jpg" width="200px">
+                        <h6 class="mb-4">R200</h6>
+                            <a href="#" class="btn btn-sn w-100 text-white custom-bg shadow-none mb-2">Add to Cart</a>
+                            <a href="#" class="btn btn-sn w-100 btn-outline-dark shadow-none">More details</a>
+                        <h5 class="mb-0 ms-0"></h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 mb-5 px-5">
+                <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+                    <div class="d-flex-row align-items-center mb-2">
+                        <img src="images/Book/e.jpg" width="200px">
+                        <h6 class="mb-4">R200</h6>
+                            <a href="#" class="btn btn-sn w-100 text-white custom-bg shadow-none mb-2">Add to Cart</a>
+                            <a href="#" class="btn btn-sn w-100 btn-outline-dark shadow-none">More details</a>
+                        <h5 class="mb-0 ms-0"></h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 mb-5 px-5">
+                <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+                    <div class="d-flex-column align-items-center mb-2">
+                        <img src="images/Book/f.jpg" width="200px">
+                        <h6 class="mb-4">R200</h6>
+                            <a href="#" class="btn btn-sn w-100 text-white custom-bg shadow-none mb-2">Add to Cart</a>
+                            <a href="#" class="btn btn-sn w-100 btn-outline-dark shadow-none">More details</a>
+                        <h5 class="mb-0 ms-0"></h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <?php require('inc/footer.php') ?>
+</body>
+
+</html>
