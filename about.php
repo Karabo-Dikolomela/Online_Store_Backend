f1b98dabc224b73b28bec7c725be2d01ba734459
Karabo-Dikolomela <108591320+Karabo-Dikolomela@users.noreply.github.com>
2022-12-12 06:13:33 +0200

    Created "about" page
    linked header, footer and links pages

---
+++ about.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BUSY HOTEL - ABOUT</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css">
+    <?php require_once('inc/links.php') ?>
+</head>
+
+<body>
+<?php require_once('inc/header.php') ?>
+
+
+<?php require('inc/footer.php') ?>
+</body>
+
+</html>
