03128fc41edaf90f0f24e41a4ea407900809388c
Karabo-Dikolomela <108591320+Karabo-Dikolomela@users.noreply.github.com>
2022-12-12 06:27:52 +0200

    insert banner/hero
    added images folder and included hero on landing page

--- index.php
+++ index.php
@@ -12,6 +12,17 @@
 <body class="bg-light m-5">
 <?php require_once('inc/header.php') ?>
 
+ <!--hero-->
+ <div class="mb-5" style="
+    background-image: url('./images/banner/hero-banner.jpg');
+    height: 30vh;
+  ">
+        <h5 class="text-white text-center shadow-1-strong p-5 text-center">"To know that we know what we know, and to know that we do not know what we do not know, that is true knowledge."
+
+            <br><br><strong>~Nicolaus Copernicus</strong>
+        </h5>
+    </div>
+
 <?php require('inc/footer.php') ?>
 </body>
 </html>
