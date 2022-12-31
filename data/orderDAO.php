35ddec336aec42e5bdaa453715b5c95b77b623ba
Karabo-Dikolomela <108591320+Karabo-Dikolomela@users.noreply.github.com>
2022-12-31 16:52:22 +0200

    Created orderDAO for proccessing orders

---
+++ data/orderDAO.php
@@ -0,0 +1,43 @@
+<?php
+
+include __DIR__ . "/config.php";
+
+class orderDAO
+{
+
+    // ========================= DB CRUD METHODS =========================
+
+
+    public static function createOrder()
+    {
+        global $connect;
+
+        // Grab SESSION data
+        $userId = $_SESSION['LoggedInUser']['id'];
+        $userCart = $_SESSION['Cart'];
+        $buyDate = date('Y/m/d');
+
+        // Begin prepare statement
+        $sql = "INSERT INTO orderss (customer_id, buy_date) VALUES (?, ?)";
+        $stmt = $connect->prepare($sql);
+
+        // Bind passed variable to prepare statement
+        $stmt->bind_param("is", $userCart, $buyDate);
+        $stmt->execute();
+
+        // Begin prepare statement
+        $sql2 = "INSERT INTO book_order (order_id, book_id) VALUES (?, ?)";
+        $stmt2 = $connect->prepare($sql2);
+        $order_id = $connect->insert_id;
+
+        // Bind passed variable to prepare statement
+        foreach ($_SESSION['Cart'] as $book_id) {
+            $stmt2->bind_param("ii", $order_id, $book_id);
+            $stmt2->execute();
+
+        }
+
+        return true;
+    }
+
+}
