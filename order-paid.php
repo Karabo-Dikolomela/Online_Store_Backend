7524f817247e9d5b2a2ba72b4a9be72f6f6b2bd6
Karabo-Dikolomela <108591320+Karabo-Dikolomela@users.noreply.github.com>
2023-01-06 18:58:04 +0200

    Order paid message

--- order-paid.php
+++ order-paid.php
@@ -7,16 +7,8 @@
 ?>
 
 <body>
-    <!-- Hero -->
-    <div class="jumbotron mt-5 p-3 p-md-5 text-white bg-dark">
-        <div class="col-md-12 px-0">
-            <h1 class="display-4 text-center mt-5">Woohoo! 🎉</h1>
-            <p class="lead my-3 text-center">I hope you're ready to play some games! <i>GG WP</i></p>
-        </div>
-    </div>
-    <!-- Hero END -->
     
-    <h2 class="my-3 text-center">Great! 😃 Please check your <u>email</u> for your game code(s)!</h2>
+    <h2 class="my-3 text-center">😃 Please check your <u>email</u> for your books!</h2>
 
     <?php require('inc/footer.php') ?>   
 
